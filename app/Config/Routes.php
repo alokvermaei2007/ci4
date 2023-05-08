9a6d98f3c08e1baffd16bf7cfb3a31da26b441df
alokvermaei2007 <alokvermaei2007@gmail.com>
2023-05-08 13:24:44 +0800

    added additional changes.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'UserCrud::index');
 
 $routes->get('users-list', 'UserCrud::index');
 $routes->get('user-form', 'UserCrud::create');
--- app/Views/add_user.php
+++ app/Views/add_user.php
@@ -1,57 +1,41 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title><?= isset($user_obj) ? 'Edit user' : 'Add User With Validation' ?></title>
+    <title>Add User With Validation </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/user.css') ?>">
+    <link rel="stylesheet"  type="text/css" href="<?= base_url('assets/css/user.css') ?>">
 </head>
 <body>
-<?php if(isset($validation)): ?>
-    <div class="alert alert-danger"><?= $validation->listErrors() ?></div>
-<?php endif; ?>
+
 <div class="container mt-5">
-    <form method="post" id="<?= isset($user_obj) ? 'update_user' : 'add_create' ?>"
-          name="<?= isset($user_obj) ? 'update_user' : 'add_create' ?>"
-          action="<?= isset($user_obj) ? site_url('/update') : site_url('/submit-form') ?>">
-        <?php if(isset($user_obj)) { ?>
-            <input type="hidden" name="id" id="id" value="<?= $user_obj['id'] ?>">
-        <?php } ?>
+    <form method="post" id="add_create" name="add_create"
+          action="<?= site_url('/submit-form') ?>">
         <div class="form-group">
             <label>First Name</label>
-            <input type="text" name="firstname" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['firstname'] : '' ?>">
+            <input type="text" name="firstname" class="form-control">
         </div>
         <div class="form-group">
             <label>Last Name</label>
-            <input type="text" name="lastname" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['lastname'] : '' ?>">
-        </div>
-        <div class="form-group">
-            <label>Email</label>
-            <input type="email" name="email" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['email'] : '' ?>">
+            <input type="text" name="lastname" class="form-control">
         </div>
         <div class="form-group">
             <label>Mobile</label>
-            <input type="text" name="mobile" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['mobile'] : '' ?>">
+            <input type="text" name="mobile" class="form-control">
         </div>
         <div class="form-group">
             <label>Username</label>
-            <input type="text" name="username" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['username'] : '' ?>">
+            <input type="text" name="username" class="form-control">
         </div>
         <div class="form-group">
             <label>Password</label>
-            <input type="password" name="password" class="form-control"
-                   value="<?= isset($user_obj) ? $user_obj['password'] : '' ?>">
+            <input type="password" name="password" class="form-control">
         </div>
         <div class="form-group">
-            <button type="submit"
-                    class="btn <?= isset($user_obj) ? 'btn-danger' : 'btn-primary' ?> btn-block">
-                <?= isset($user_obj) ? 'Save Data' : 'Update Data' ?>
-            </button>
+            <label>Email</label>
+            <input type="text" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Update Data</button>
         </div>
     </form>
 </div>
